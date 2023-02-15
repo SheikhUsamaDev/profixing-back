c9fdd7fb2f62ef4bb60d96db720d0cc90613119f
Ahsan Faisal <61789531+AUAboi@users.noreply.github.com>
2023-02-15 17:53:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const offers = require("./Routes/offers");
 var cors = require("cors");
 App.use(cors());
 App.use(cookieParser());
-mongoose.connect("mongodb+srv://Rizwan:Rizwanmirza1@cluster0.lv75msq.mongodb.net/profixingDB?retryWrites=true&w=majority").then(console.log("Your Connection is Successful"));
+mongoose.connect(process.env.MONGO_URI).then(console.log("Your Connection is Successful"));
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "./server/config/.env" });
 }
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 App.use(express.json());
 App.use(
   bodyParser.urlencoded({
